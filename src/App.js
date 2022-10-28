448cae827aaee55057b9b71c5a2925ef108a5335
RobD01 <medbz2k3@gmail.com>
2022-10-27 22:22:19 -0700

    remove basename

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from "./Pages/Home";
 
 const App = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
