badb643ef88568711dda7a28b38cbaaf7a7d269a
RobD01 <medbz2k3@gmail.com>
2022-10-28 20:11:46 -0700

    homepage links

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,8 +8,17 @@ const Home = () => {
   const githubIcon =
     "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png";
   const github = "http://github.com/RobD01";
+
   const youtubeIcon = "https://cdn-icons-png.flaticon.com/512/1384/1384060.png";
   const youtube = "https://www.youtube.com/channel/UCw-d1MWiu7c5F42afsodrKQ";
+
+  const patreon = "https://patreon.com/moneytheory";
+  const PatreonIcon =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Patreon_logo.svg/800px-Patreon_logo.svg.png";
+
+  const coffee = "https://www.buymeacoffee.com/moneytheorF";
+  const CoffeeIcon =
+    "https://user-images.githubusercontent.com/16066404/77041853-a2044100-69e0-11ea-8da6-d64822a2c72a.jpg";
 
   return (
     <div className="container">
@@ -27,16 +36,24 @@ const Home = () => {
           back-end
         </p>
         <p>My other passions include finance and cinematography</p>
-        <p>My Github and Youtube portfolio below:</p>
+        <p>My Github, Youtube, Patreon, and Buy Me Coffee below:</p>
       </div>
 
       {/* Portfolio links */}
-      <div className="my-2 d-flex justify-content-center">
-        <a href={github} target="_blank" rel="noreferrer">
+      <div className="my-2 mx-5 d-flex justify-content-center flex-wrap">
+        <a href={github} target="_blank" rel="noreferrer" className="m-3">
           <img src={githubIcon} alt="Github" height="100" />
         </a>
-        <a href={youtube} target="_blank" rel="noreferrer">
+        <a href={youtube} target="_blank" rel="noreferrer" className="m-3">
           <img src={youtubeIcon} alt="Youtube" height="100" />
+        </a>
+
+        <a href={patreon} target="_blank" rel="noreferrer" className="m-3">
+          <img src={PatreonIcon} alt="Patreon" height="100" />
+        </a>
+
+        <a href={coffee} target="_blank" rel="noreferrer" className="m-3">
+          <img src={CoffeeIcon} alt="Coffee" height="100" />
         </a>
       </div>
     </div>
